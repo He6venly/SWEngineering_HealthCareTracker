cef2db4fdb4ef0a80d27238946721da98cd6af9f
SH.Ahn <dkstjsgy1@naver.com>
2026-05-27 17:04:32 +0900

    공통 API 응답 객체 구현

--- CWNU14_HealthCare/src/main/java/cwnu/healthcare/global/common/ApiResponse.java
+++ CWNU14_HealthCare/src/main/java/cwnu/healthcare/global/common/ApiResponse.java
@@ -1,4 +1,21 @@
 package cwnu.healthcare.global.common;
 
-public class ApiResponse {
+public record ApiResponse<T>(
+	int status,
+	String message,
+	String errorCode,
+	T data
+) {
+
+	public static <T> ApiResponse<T> success(T data) {
+		return new ApiResponse<>(200, "요청이 성공적으로 처리되었습니다.", null, data);
+	}
+
+	public static ApiResponse<Void> success() {
+		return new ApiResponse<>(200, "요청이 성공적으로 처리되었습니다.", null, null);
+	}
+
+	public static ApiResponse<Void> error(int status, String message, String errorCode) {
+		return new ApiResponse<>(status, message, errorCode, null);
+	}
 }
