5444710985757492db7a1eda24cb242bed8166f9
hwang030915 <hwang45408016@gmail.com>
2026-05-28 15:51:12 +0900

    profile: 프로필 생성 및 수정을 위한 HealthProfileRequest DTO 구현

--- CWNU14_HealthCare/src/main/java/cwnu/healthcare/domain/profile/dto/HealthProfileRequest.java
+++ CWNU14_HealthCare/src/main/java/cwnu/healthcare/domain/profile/dto/HealthProfileRequest.java
@@ -1,4 +1,22 @@
 package cwnu.healthcare.domain.profile.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class HealthProfileRequest {
+    private String userId;
+
+    private double height;
+
+    private double weight;
+
+    private int targetCalories;
+
+    private double targetWeight;
 }
