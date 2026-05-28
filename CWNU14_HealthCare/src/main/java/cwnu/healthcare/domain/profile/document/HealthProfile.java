7dab245c281fd560cf824d7307c23af56c797c62
hwang030915 <hwang45408016@gmail.com>
2026-05-28 15:35:06 +0900

    profile: MongoDB 매핑을 위한 HealthProfile document entity 구현

--- CWNU14_HealthCare/src/main/java/cwnu/healthcare/domain/profile/document/HealthProfile.java
+++ CWNU14_HealthCare/src/main/java/cwnu/healthcare/domain/profile/document/HealthProfile.java
@@ -1,4 +1,31 @@
 package cwnu.healthcare.domain.profile.document;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "health_profiles")
 public class HealthProfile {
+
+    @Id
+    private String id;
+
+    @Indexed
+    private String userId;
+
+    private double height;
+
+    private double weight;
+
+    private int targetCalories;
+
+    private double targetWeight;
 }
