738e9ce8e9b51be96059055c23e95e5816a1f261
hwang030915 <hwang45408016@gmail.com>
2026-06-02 14:10:44 +0900

    profile: HealthProfileRequest 내 userId 필드 제거 및 @Positive 유효성 검증 추가

--- CWNU14_HealthCare/src/main/java/cwnu/healthcare/domain/profile/dto/HealthProfileRequest.java
+++ CWNU14_HealthCare/src/main/java/cwnu/healthcare/domain/profile/dto/HealthProfileRequest.java
@@ -1,5 +1,7 @@
 package cwnu.healthcare.domain.profile.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,11 +12,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class HealthProfileRequest {
+    @NotNull
+    @Positive
     private double height;
 
+    @NotNull
+    @Positive
     private double weight;
 
+    @NotNull
+    @Positive
     private int targetCalories;
 
+    @NotNull
+    @Positive
     private double targetWeight;
 }
