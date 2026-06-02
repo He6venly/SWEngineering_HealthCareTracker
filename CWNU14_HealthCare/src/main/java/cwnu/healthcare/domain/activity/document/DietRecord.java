b7de32157bcdb32628c5d7dc7ec05eccb6a46a7a
hwang030915 <hwang45408016@gmail.com>
2026-06-03 05:56:32 +0900

    activity: DietRecord 문서 구조 정의

--- CWNU14_HealthCare/src/main/java/cwnu/healthcare/domain/activity/document/DietRecord.java
+++ CWNU14_HealthCare/src/main/java/cwnu/healthcare/domain/activity/document/DietRecord.java
@@ -1,4 +1,43 @@
 package cwnu.healthcare.domain.activity.document;
 
-public class DietRecord {
+import cwnu.healthcare.domain.activity.type.MealType;
+import cwnu.healthcare.global.common.BaseDocument;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Document(collection = "diet_records")
+public class DietRecord extends BaseDocument {
+
+    @Id
+    private String id;
+
+    @Indexed
+    private String userId;
+
+    private MealType mealType;
+
+    private String foodName;
+
+    private int calories;
+
+    @Indexed
+    private LocalDate recordDate;
+
+    @Builder
+    public DietRecord(String userId, MealType mealType, String foodName, int calories, LocalDate recordDate) {
+        this.userId = userId;
+        this.mealType = mealType;
+        this.foodName = foodName;
+        this.calories = calories;
+        this.recordDate = recordDate;
+    }
 }
