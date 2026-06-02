16d1054f1e57db826f697c993f084ca938d3d566
hwang030915 <hwang45408016@gmail.com>
2026-06-03 06:08:27 +0900

    activity: ActivityResponse 응답 구조 정의

--- CWNU14_HealthCare/src/main/java/cwnu/healthcare/domain/activity/dto/ActivityResponse.java
+++ CWNU14_HealthCare/src/main/java/cwnu/healthcare/domain/activity/dto/ActivityResponse.java
@@ -1,4 +1,35 @@
 package cwnu.healthcare.domain.activity.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import cwnu.healthcare.domain.activity.type.ExerciseSource;
+import cwnu.healthcare.domain.activity.type.MealType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ActivityResponse {
+
+    private String recordId;
+
+    private String type;
+
+    private String name;
+
+    private int calories;
+
+    private Integer durationMinutes;
+
+    private MealType mealType;
+
+    private ExerciseSource source;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate recordDate;
 }
