8e86c2cd8146399abdc2de6eb674e69d582e385e
hwang030915 <hwang45408016@gmail.com>
2026-06-03 06:42:26 +0900

    simulation: WearableSimulationResponse 응답 구조 정의

--- CWNU14_HealthCare/src/main/java/cwnu/healthcare/domain/simulation/dto/WearableSimulationResponse.java
+++ CWNU14_HealthCare/src/main/java/cwnu/healthcare/domain/simulation/dto/WearableSimulationResponse.java
@@ -1,4 +1,27 @@
 package cwnu.healthcare.domain.simulation.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class WearableSimulationResponse {
+
+    private String syncStatus;
+
+    private int processedCaloriesBurned;
+
+    private int durationMinutes;
+
+    private String exerciseRecordId;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate recordDate;
 }
