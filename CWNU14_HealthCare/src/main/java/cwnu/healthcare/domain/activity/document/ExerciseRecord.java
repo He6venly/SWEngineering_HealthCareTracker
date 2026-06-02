2e7ff2938d21fc107c932c8f38fabda0473462c9
hwang030915 <hwang45408016@gmail.com>
2026-06-03 05:58:54 +0900

    activity: ExerciseRecord 문서 구조 정의

--- CWNU14_HealthCare/src/main/java/cwnu/healthcare/domain/activity/document/ExerciseRecord.java
+++ CWNU14_HealthCare/src/main/java/cwnu/healthcare/domain/activity/document/ExerciseRecord.java
@@ -1,4 +1,47 @@
 package cwnu.healthcare.domain.activity.document;
 
-public class ExerciseRecord {
+import cwnu.healthcare.domain.activity.type.ExerciseSource;
+import cwnu.healthcare.global.common.BaseDocument;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Document(collection = "exercise_records")
+public class ExerciseRecord extends BaseDocument {
+
+    @Id
+    private String id;
+
+    @Indexed
+    private String userId;
+
+    private String exerciseName;
+
+    private int durationMinutes;
+
+    private int caloriesBurned;
+
+    private ExerciseSource source;
+
+    @Indexed
+    private LocalDate recordDate;
+
+    @Builder
+    public ExerciseRecord(String userId, String exerciseName, int durationMinutes,
+                          int caloriesBurned, ExerciseSource source, LocalDate recordDate) {
+        this.userId = userId;
+        this.exerciseName = exerciseName;
+        this.durationMinutes = durationMinutes;
+        this.caloriesBurned = caloriesBurned;
+        this.source = source;
+        this.recordDate = recordDate;
+    }
 }
